df12e4cbb0486043e097d98b9d358bbf7411d99d
alberttan123 <attain938@gmail.com>
2025-11-06 21:06:29 +0800

    implement login and signup functionality

---
+++ DatabaseConnection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WAPP_Assignment
+{
+    public class DataAccess
+    {
+        private static readonly string ConnStr =
+        ConfigurationManager.ConnectionStrings["main"].ConnectionString;
+
+        public static SqlConnection GetOpenConnection()
+        {
+            var conn = new SqlConnection(ConnStr);
+            conn.Open();
+            return conn;
+        }
+    }
+}
